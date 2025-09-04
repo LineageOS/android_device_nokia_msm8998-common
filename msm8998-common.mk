fa69aef567fc9174c7890fda96ff4376287dfde9
Łukasz Patron <priv.luk@gmail.com>
2025-09-04 07:49:09 +0000

    msm8998-common: Migrate to AIDL LiveDisplay HAL
    Change-Id: I4e8be836e94d442b40c63a1ac7dade3c9d9c784c

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -198,7 +198,7 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
+    vendor.lineage.livedisplay-service.sdm
 
 # Low-Power whitelist
 PRODUCT_COPY_FILES += \
