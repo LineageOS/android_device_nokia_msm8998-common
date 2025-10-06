fea40573e447a8470bd792c77ad17289a21b9d09
Łukasz Patron <priv.luk@gmail.com>
2025-10-06 04:14:19 +0000

    msm8998-common: Migrate to AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d

--- msm8998-common.mk
+++ msm8998-common.mk
@@ -69,8 +69,7 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider-service_32.lineage
 
 # Charger
 PRODUCT_PACKAGES += \
